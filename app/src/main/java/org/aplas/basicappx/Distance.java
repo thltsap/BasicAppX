8fd21cbfeab88fb9562a89b732f5a233d356bf34
ThalitaSaniyyaAqillaPutri <79181196+ThalitaSaniyyaAqillaPutri@users.noreply.github.com>
2021-09-20 11:10:43 +0700

    Module B1X.02

---
+++ app/src/main/java/org/aplas/basicappx/Distance.java
@@ -0,0 +1,68 @@
+package org.aplas.basicappx;
+
+public class Distance {
+
+    private double meter;
+
+    public Distance(){
+        this.meter = 0;
+    }
+    public void setMeter(double meter){
+     this.meter=meter;
+    }
+    public void setInch(double meter){
+        this.meter = meter / 39.3701;
+    }
+    public void setMile(double meter){
+        this.meter = meter / 0.000621371;
+    }
+    public void setFoot(double meter){
+        this.meter = meter / 3.28084;
+    }
+    public double getMeter(){
+        return meter;
+    }
+    public double getInch(){
+        return meter * 39.3701;
+    }
+    public double getMile(){
+        return meter * 0.000621371;
+    }
+    public double getFoot(){
+        return meter * 3.28084;
+    }
+    public double convert(String oriUnit, String convUnit, double value) {
+        switch (oriUnit) {
+            case "Mtr":
+                setMeter(value);
+                break;
+            case "Inc":
+                setInch(value);
+                break;
+            case "Mil":
+                setMile(value);
+                break;
+            case "Ft":
+                setFoot(value);
+                break;
+        }
+
+        double result = 0;
+        switch (convUnit) {
+            case "Mtr":
+                result = getMeter();
+                break;
+            case "Inc":
+                result = getInch();
+                break;
+            case "Mil":
+                result = getMile();
+                break;
+            case "Ft":
+                result = getFoot();
+                break;
+        }
+
+        return result;
+    }
+}
